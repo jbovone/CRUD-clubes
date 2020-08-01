df3c0ae7ecd6e88c17419ab83eb6c532991fb6e5
jbovone <58782618+jbovone@users.noreply.github.com>
2020-07-31 23:30:26 -0300

    Revert "commit"
    This reverts commit 134330fe35eb89456ea1f80f993810cc8e8b0b52.

--- src/entities/team.js
+++ src/entities/team.js
@@ -1,15 +1,8 @@
-class Team {
-  constructor(name, crestUrl, venue, founded, phone, website, email, clubcolors, seedList, id) {
+module.exports = class Team {
+  constructor(name, id, img, venue) {
     this.name = name;
-    this.crestUrl = crestUrl;
-    this.venue = venue;
-    this.founded = founded;
-    this.phone = phone;
-    this.website = website;
-    this.email = email;
-    this.clubcolors = clubcolors;
-    this.seedList = seedList;
     this.id = id;
+    this.crest = img;
+    this.venue = venue;
   }
-}
-module.exports = Team;
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const KEYS = require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -9,12 +8,8 @@ const hbs = exphbs.create({
   defaultLayout: 'index',
   layoutsDir: path.join(__dirname, 'public', 'layouts'),
   partialsDir: path.join(__dirname, 'public', 'partials'),
-  helpers: {
-    paginatorPage(previous) {
-      return previous + 1
-    }
-  },
 });
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'public'));
@@ -23,4 +18,4 @@ app.set('view engine', '.hbs');
 route(app, express);
 
 const PORT = 8080;
-app.listen(process.env.PORT || PORT, () => console.log('running 8080', KEYS));
+app.listen(process.env.PORT || PORT);
